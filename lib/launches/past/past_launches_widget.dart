26143d9f322b3831b153346aa7b9222cce131bc1
MinaHamdy <Mina.Hamdy@lavaloon.com>
2021-03-15 21:56:14 +0200

    Fix Local Import URL

--- lib/launches/past/past_launches_widget.dart
+++ lib/launches/past/past_launches_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:space_x/launches/past/past_launches_controller.dart';
 import 'package:space_x/ui_components/date_time_filter_widget.dart';
-import 'file:///C:/Users/MinaHamdy/FlutterProjects/space_x/lib/launches/launches_list_item_widget.dart';
+import 'package:space_x/launches/launches_list_item_widget.dart';
 
 class PastLaunchesWidget extends StatelessWidget {
   PastLaunchesController controller;
